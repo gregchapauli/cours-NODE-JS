d24fa8e1cc202363e35bcb91d26c8ed163045747
Gregory Caloin <gregchapauli@gmail.com>
2020-09-18 21:50:56 +0200

    CH4

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var a = 1 + 2;
+
+var funcX = function(x) {
+    return x * 2;
+}
+
+console.log(a);
+console.log(funcX(1));
---
+++ operations.js
@@ -0,0 +1,17 @@
+let a = 1 + 2;
+const b = 1 - 1;
+const c = 2 / 2;
+const d = 1 * 2;
+
+console.log(a, b, c, d);
+
+a = 2 + 2;
+
+console.log(a);
+
+var funcX = function(x) {
+    return x * 2;
+}
+
+console.log(funcX(1));
+console.log(funcX(2));
